4c9cc4654af117f82c8d6ff09c4095d45018c904
dsykex <dezmensykes.ds@gmail.com>
2019-07-22 16:36:08 -0400

    Initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+declare var TimelineMax: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage {
+  public post: any = {};
+  
+  constructor() {
+    console.log(TimelineMax);
+  }
 
 }
